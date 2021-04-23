097544e933b3e93c00b3bcbfa4155c237b163e6d
alena424 <xtesar36@stud.fit.vutbr.cz>
2021-04-23 12:02:56 +0200

    Fix title

--- src/App.tsx
+++ src/App.tsx
@@ -110,7 +110,7 @@ const App: React.FC = () => {
 
     return (
         <div className="App container">
-            <h1 className="title">BIN - visualization cellular automata</h1>
+            <h1 className="title">BIN - cellular automata visualization</h1>
             <h4 className="subtitle">Alena Tesařová (xtesar36@stud.fit.vutbr.cz)</h4>
             <AutomataConfig
                 boardWidth={boardWidth}
